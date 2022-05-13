458bfcf476282a3717803588bc42b9b160c18350
AshiqRhn <71887159+AshiqRhn@users.noreply.github.com>
2022-05-13 17:43:49 -0400

    Review
    Fixed bugs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
-import { Box, Container, Grid, Typography } from "@mui/material";
-import type { NextPage } from "next";
+import type { NextPage } from 'next';
 import Users from "../src/Users";
 
 const Home: NextPage = () => {
   return (
-    <>
-    
+    <>    
       <Users />
     </>
   );
